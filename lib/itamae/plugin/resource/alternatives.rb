e80b297f68a1e9a76a0273dd0a516b2e73e46718
Yuya.Nishida <yuya@j96.org>
2016-05-25 19:44:52 +0900

    run "bundle gem itamae-plugin-resource-alternatives".

---
+++ lib/itamae/plugin/resource/alternatives.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/resource/alternatives/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module Alternatives
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/resource/alternatives/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Resource
+      module Alternatives
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
