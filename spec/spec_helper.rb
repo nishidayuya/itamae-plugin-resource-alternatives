a212ae251736688525ae95ad4de6dc649d83504f
Yuya.Nishida <yuya@j96.org>
2016-06-25 00:08:42 +0900

    more simplify.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,45 +1,27 @@
-require 'serverspec'
-require 'net/ssh'
-require 'tempfile'
+require "tempfile"
+require "open3"
+
+require "serverspec"
+require "net/ssh"
+
+def run(*args)
+  Bundler.with_clean_env do
+    stdout, status = *Open3.capture2(*args)
+    if !status.success?
+      raise "failure: #{args.inspect}"
+    end
+    return stdout
+  end
+end
+
+host = ENV["TARGET_HOST"]
+run("vagrant up #{host}")
+options = Tempfile.open("") do |f|
+  f.write(run("vagrant ssh-config #{host}"))
+  f.close
+  Net::SSH::Config.for(host, [f.path])
+end
 
 set :backend, :ssh
-
-if ENV['ASK_SUDO_PASSWORD']
-  begin
-    require 'highline/import'
-  rescue LoadError
-    fail "highline is not available. Try installing it."
-  end
-  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
-else
-  set :sudo_password, ENV['SUDO_PASSWORD']
-end
-
-host = ENV['TARGET_HOST']
-
-Bundler.with_clean_env do
-  `vagrant up #{host}`
-end
-
-config = Tempfile.new('', Dir.tmpdir)
-Bundler.with_clean_env do
-  config.write(`vagrant ssh-config #{host}`)
-end
-config.close
-
-options = Net::SSH::Config.for(host, [config.path])
-
-options[:user] ||= Etc.getlogin
-
-set :host,        options[:host_name] || host
+set :host, options[:host_name] || host
 set :ssh_options, options
-
-# Disable sudo
-# set :disable_sudo, true
-
-
-# Set environment variables
-# set :env, :LANG => 'C', :LC_MESSAGES => 'C'
-
-# Set PATH
-# set :path, '/sbin:/usr/local/sbin:$PATH'
